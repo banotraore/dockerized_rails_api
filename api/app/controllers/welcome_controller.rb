a7a5449c45da6116dbb948fdc3ef1cdbe2166ead
Bano Traore <47910631+banotraore@users.noreply.github.com>
2021-11-17 22:38:39 +0100

    welcome controller removed and typo on user serializer corrected

--- api/app/controllers/welcome_controller.rb
+++
@@ -1,5 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-    render json: { message: 'Welcome to the index' }, status: :ok
-  end
-end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
   resources :users, only: [:index]
   resources :posts, only: [:index]
-
-  get 'welcome/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
